58e908c7378bcd103f0dd50a9251440911b5bfe1
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-02-23 21:39:30 +0500

    Add files via upload

---
+++ 12 Names.ts
@@ -0,0 +1,8 @@
+let names :string[] = ['Khansa','Rohaan','Abdul Rafay','Abdul Rahman'] ;
+
+const message :string = 'Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+}
