5916ae3844bcbc18ca68cb7413679e43638f24e2
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-02-24 10:58:05 +0500

    Add files via upload

---
+++ 20 Empty_Array.ts
@@ -0,0 +1,11 @@
+
+
+let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+let item2 :string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
