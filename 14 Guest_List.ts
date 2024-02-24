715aeb32ee19b91d99b833c73aac3ef8131fa7e7
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-02-24 10:51:13 +0500

    Add files via upload

---
+++ 14 Guest_List.ts
@@ -0,0 +1,7 @@
+let Guest_List :string[] = ['Nawaz Sharif','Imran Khan','Bilawal Bhutto'] ;
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party. \n\nThank You!')
+
+}
