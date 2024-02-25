f5350088c88c89095cd6e5565ecadaef237844fc
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-02-25 18:09:26 +0500

    Add files via upload

---
+++ 22 International_Error.ts
@@ -0,0 +1,11 @@
+
+//Creating Array
+let newArray :string[] = ['Will Smith', 'James Bond', 'Tom Cruise'];
+
+//Accessing Array by index.
+console.log(newArray[5]);
+
+//Make sure u correct the error before closing the program.
+
+//Correct the error
+console.log(newArray);
